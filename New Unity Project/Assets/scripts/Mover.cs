c3018328f196054580484735c948d48c5d470cea
LORDS\Benjamin <bchouteau1@yahoo.com>
2018-01-21 21:49:02 -0700

    Comments
    Added more comments

--- New Unity Project/Assets/scripts/Mover.cs
+++ New Unity Project/Assets/scripts/Mover.cs
@@ -8,21 +8,28 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        //getting the Input
+        //getting the Input for left arrow key
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            //moving the sprite
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
+        //getting the Input for right arrow key
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            //moving the sprite
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
+        //getting the Input for up arrow key
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            //moving the sprite
             transform.position += Vector3.up * speed * Time.deltaTime;
         }
+        //getting the Input for down arrow key
         if (Input.GetKey(KeyCode.DownArrow))
         {
+            //moving the sprite
             transform.position += Vector3.down * speed * Time.deltaTime;
         }
     }
