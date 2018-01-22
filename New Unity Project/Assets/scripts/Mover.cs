28cb13f5b00737bbaa854c1a096fa3e6ba95c723
LORDS\Benjamin <bchouteau1@yahoo.com>
2018-01-21 21:38:13 -0700

    PT1
    !st part of the project

---
+++ New Unity Project/Assets/scripts/Mover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    // setting the speed that the sprite is allowed to move
+    public float speed = 1.5f;
+
+    void Update()
+    {
+        //getting the Input
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
